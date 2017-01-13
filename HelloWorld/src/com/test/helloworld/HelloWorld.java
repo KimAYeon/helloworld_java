c91868c3c9b5dbff2d22691521d0516a4c939a51
마구리 <zetn333@naver.com>
2017-01-13 14:12:01 +0900

    초기 버전

--- HelloWorld/src/com/test/helloworld/HelloWorld.java
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -1,11 +1,8 @@
 package com.test.helloworld;
 
 public class HelloWorld {
-	
 	public static void main(String arg[]) {
 		System.out.println("Hello, World~!");
 		System.out.println("�ȳ��ϼ���");
 	}
-	
-
 }
